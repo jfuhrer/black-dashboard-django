70c31170c96044afe1d4501114a2c837845398cf
jfuhrer <jara.fuhrer@hotmail.com>
2021-05-05 11:03:11 +0200

    popup note draggable

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ import datetime
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 from django import template
 from django.urls import reverse_lazy
@@ -104,7 +105,7 @@ def index(request):
         return render(request, "index.html", context)
 
     except template.TemplateDoesNotExist:
-        html_template = loader.get_template( 'page-404.html' )
+        html_template = loader.get_template('page-404.html')
         return HttpResponse(html_template.render(context, request))
 
     #html_template = loader.get_template( 'index.html' )
@@ -118,20 +119,20 @@ def pages(request):
     # Pick out the html file name from the url. And load that template.
     try:
 
-        load_template      = request.path.split('/')[-1]
+        load_template = request.path.split('/')[-1]
         context['segment'] = load_template
 
-        html_template = loader.get_template( load_template )
+        html_template = loader.get_template(load_template)
         return HttpResponse(html_template.render(context, request))
 
     except template.TemplateDoesNotExist:
 
-        html_template = loader.get_template( 'page-404.html' )
+        html_template = loader.get_template('page-404.html')
         return HttpResponse(html_template.render(context, request))
 
     except:
 
-        html_template = loader.get_template( 'page-500.html' )
+        html_template = loader.get_template('page-500.html')
         return HttpResponse(html_template.render(context, request))
 
 
@@ -155,4 +156,3 @@ def notes(request):
     except:
         html_template = loader.get_template('page-500.html')
         return HttpResponse(html_template.render(context, request))
-
