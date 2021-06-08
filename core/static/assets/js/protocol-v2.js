6128e1b3b14ccb2d3f212443b9783312f2376d43
jfuhrer <jara.fuhrer@hotmail.com>
2021-06-08 18:11:14 +0200

    protocol up down arrows

--- core/static/assets/js/protocol-v2.js
+++ core/static/assets/js/protocol-v2.js
@@ -15,12 +15,35 @@ function openAll() {
     for (var i = 0; i < divs.length; ++i) {
         divs[i].style.display = "block"
     }
+
+
+   var iconsDown = document.getElementsByClassName('toggleDown');
+   for (var i = 0; i < iconsDown.length; ++i) {
+      iconsDown[i].style.display = "none";
+   }
+
+   var iconsUp = document.getElementsByClassName('toggleUp');
+   for (var i = 0; i < iconsUp.length; ++i) {
+      iconsUp[i].style.display = "contents";
+   }
+
+
 }
 
 function closeAll() {
    var divs = document.getElementsByClassName('details');
    for (var i = 0; i < divs.length; ++i) {
       divs[i].style.display = "none";
+   }
+
+   var iconsDown = document.getElementsByClassName('toggleDown');
+   for (var i = 0; i < iconsDown.length; ++i) {
+      iconsDown[i].style.display = "contents";
+   }
+
+   var iconsUp = document.getElementsByClassName('toggleUp');
+   for (var i = 0; i < iconsUp.length; ++i) {
+      iconsUp[i].style.display = "none";
    }
 }
 
