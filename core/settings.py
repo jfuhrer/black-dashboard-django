eb127f3d51bbc444386ad1f79eea63b0dbf57c6d
jfuhrer <jara.fuhrer@hotmail.com>
2021-06-16 09:05:23 +0200

    debug false

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
-DEBUG = True
+DEBUG = False
 # load production server from .env
 ALLOWED_HOSTS = ['fincon-agent.herokuapp.com', 'localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
 
